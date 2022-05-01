24ae192c68bd994a04cbbd3f586537a181ee6045
stevenEno <steven.e.eno@gmail.com>
2022-04-30 19:36:40 -0700

    Layout of frames, cells, and basic left click and right click functionality.

---
+++ cell.py
@@ -0,0 +1,69 @@
+from tkinter import Button
+import random
+import settings
+
+class Cell:
+    all = []
+    def __init__(self,x, y, is_mine=False):
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Append the object to the Cell.all list
+        Cell.all.append(self)
+
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            width=12,
+            height=4,
+            text=f'{self.x},{self.y}'
+        )
+        btn.bind('<Button-1>', self.left_click_actions) # Left Click
+        btn.bind('<Button-3>', self.right_click_actions)
+        self.cell_btn_object = btn
+
+    def left_click_actions(self, event):
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axis(self, x, y):
+        # Return a cell object based on the values of x, y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    def show_cell(self):
+        surrounded_cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        print(surrounded_cells)
+
+    def show_mine(self):
+        # A logic to interrupte the game and display a message that player lost!
+        self.cell_btn_object.configure(bg='red')
+
+    def right_click_actions(self, event):
+        print(event)
+        print("I am right clicked!")
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all, settings.MINES_COUNT
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self):
+        return f'Cell({self.x}, {self.y})'
