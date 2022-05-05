aa79d7f12b64001e27aba3bdf08fda45e5974081
stevenEno <steven.e.eno@gmail.com>
2022-05-05 12:21:37 -0700

    Added number of mines surrounding a cell functionality.

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+from re import S
 from tkinter import Button
 import random
 import settings
@@ -17,8 +18,7 @@ class Cell:
         btn = Button(
             location,
             width=12,
-            height=4,
-            text=f'{self.x},{self.y}'
+            height=4
         )
         btn.bind('<Button-1>', self.left_click_actions) # Left Click
         btn.bind('<Button-3>', self.right_click_actions)
@@ -36,8 +36,9 @@ class Cell:
             if cell.x == x and cell.y == y:
                 return cell
 
-    def show_cell(self):
-        surrounded_cells = [
+    @property
+    def surrounded_cells(self):
+        cells = [
             self.get_cell_by_axis(self.x - 1, self.y - 1),
             self.get_cell_by_axis(self.x - 1, self.y),
             self.get_cell_by_axis(self.x - 1, self.y + 1),
@@ -47,7 +48,20 @@ class Cell:
             self.get_cell_by_axis(self.x + 1, self.y + 1),
             self.get_cell_by_axis(self.x, self.y + 1)
         ]
-        print(surrounded_cells)
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+
+        return counter
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
 
     def show_mine(self):
         # A logic to interrupte the game and display a message that player lost!
